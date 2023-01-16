d97c52afafb3d1810da1ec107f33cc8a593a124e
Felipe Moiseis <083felype@gmail.com>
2023-01-16 18:28:09 -0300

    Página de cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import SignIn from "../pages/SignIn";
 import SingUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 const RoutesApp = () => {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/register" element={<SingUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
