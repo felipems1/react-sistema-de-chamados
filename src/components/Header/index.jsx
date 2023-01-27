b3fa755840bc2f2b059e5904bc30ced581f4f2c2
Felipe Moiseis <083felype@gmail.com>
2023-01-26 23:32:25 -0300

    Criando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth";
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+import "./header.css";
+import avatarImg from "../../assets/avatar.png";
+
+const Header = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="sidebar">
+      <div>
+        <img
+          src={user.avatarUrl === null ? avatarImg : user.avatarUrl}
+          alt="Foto do usuario"
+        />
+      </div>
+      <Link to="/dashboard">
+        <FiHome color="#fff" size={24} />
+        Chamados
+      </Link>
+      <Link to="/customers">
+        <FiUser color="#fff" size={24} />
+        Clientes
+      </Link>
+      <Link to="/profile">
+        <FiSettings color="#fff" size={24} />
+        Perfil
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
+import Header from "../../components/Header";
+
 const Dashboard = () => {
   const { logout } = useContext(AuthContext);
 
@@ -10,6 +12,7 @@ const Dashboard = () => {
 
   return (
     <div>
+      <Header />
       <h1>Página Dashboard</h1>
       <button onClick={handleLogout}>Sair da conta</button>
     </div>
