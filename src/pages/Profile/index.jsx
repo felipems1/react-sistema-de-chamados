1ed8f44eebbfea8d7281ed01ca098a4bea521d5d
Felipe Moiseis <083felype@gmail.com>
2023-01-30 20:39:39 -0300

    Criando Perfil

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,16 @@
+import { useState, useContext } from "react";
+import { FiSettings, FiUpload } from "react-icons/fi";
+import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
-import { FiSettings } from "react-icons/fi";
+import avatar from "../../assets/avatar.png";
 import "./profile.css";
 
 const Profile = () => {
+  const { user } = useContext(AuthContext);
+
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
   return (
     <div>
       <Header />
@@ -11,8 +18,40 @@ const Profile = () => {
         <Title name="Minha conta">
           <FiSettings size={25} />
         </Title>
+        <div className="container">
+          <form className="form-profile">
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#fff" size={25} />
+              </span>
+              <input type="file" accept="image/*" /> <br />
+              {avatarUrl === null ? (
+                <img
+                  src={avatar}
+                  alt="foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              ) : (
+                <img
+                  src={avatarUrl}
+                  alt="foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+            <label>Nome</label>
+            <input type="text" placeholder="Seu nome" />
+            <label>Email</label>
+            <input type="text" placeholder="teste@gmail.com" disabled={true} />
+            <button type="submit">Salvar</button>
+          </form>
+        </div>
+        <div className="container">
+          <button className="logout-btn">Sair</button>
+        </div>
       </div>
-      <h1>Página perfil</h1>
     </div>
   );
 };
