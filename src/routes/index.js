1d1441d4cdcc78b4a80f6367414a8e575013e4e3
Felipe Moises <083felype@gmail.com>
2023-02-14 10:40:29 -0300

    Página novo chamado

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import SingUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
 import Customers from "../pages/Customers";
+import New from "../pages/New";
 
 import Private from "./Private";
 
@@ -34,6 +35,14 @@ const RoutesApp = () => {
         element={
           <Private>
             <Customers />
+          </Private>
+        }
+      />
+      <Route
+        path="/new"
+        element={
+          <Private>
+            <New />
           </Private>
         }
       />
