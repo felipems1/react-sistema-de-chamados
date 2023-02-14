3d6eb89bcc8da18bd1b1c9438d08b309ed1c53c0
Felipe Moises <083felype@gmail.com>
2023-02-13 22:32:04 -0300

    Estilizando tabela

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -22,46 +22,51 @@ const Dashboard = () => {
         <Title name="Tickets">
           <FiMessageSquare size={25} />
         </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#fff" size={25} />
+            Novo chamado
+          </Link>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrando em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: "#999" }}>
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#3583f6" }}
+                  >
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#f6a935" }}
+                  >
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
       </div>
-      <>
-        <Link to="/new" className="new">
-          <FiPlus color="#fff" size={25} />
-          Novo chamado
-        </Link>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">Cliente</th>
-              <th scope="col">Assunto</th>
-              <th scope="col">Status</th>
-              <th scope="col">Cadastrando em</th>
-              <th scope="col">#</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td data-label="Cliente">Mercado esquina</td>
-              <td data-label="Assunto">Suporte</td>
-              <td data-label="Status">Em aberto</td>
-              <td data-label="Cadastrado">12/05/2022</td>
-              <td data-label="#">
-                <button
-                  className="action"
-                  style={{ backgroundColor: "#3583f6" }}
-                >
-                  <FiSearch color="#fff" size={17} />
-                </button>
-                <button
-                  className="action"
-                  style={{ backgroundColor: "#f6a935" }}
-                >
-                  <FiEdit2 color="#fff" size={17} />
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </>
     </div>
   );
 };
